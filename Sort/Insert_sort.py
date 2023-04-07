027dee552b7a4aa4700a6804b7d4e4725646e189
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-07 11:20:13 +0800

    Update Insert_sort.py

--- Sort/Insert_sort.py
+++ Sort/Insert_sort.py
@@ -2,9 +2,20 @@ from cal_time import *
 
 @cal_time
 def insert_sort(li):
-	temp = li[0]
 	for i in range(1, len(li)):
 		temp = li[i]
-		j = i
+		j = i-1
 		while j>=0:
-			if temp <= li[j]:
+			if temp >= li[j]:
+				li[j+1] = temp
+				break
+			else:
+				li[j+1] = li[j]
+			j -= 1
+		if j == -1:
+			li[0] = temp
+			
+li = [int(i) for i in input('Please enter the list you want to sort:').split()]
+print(li)
+insert_sort(li)
+print(li)
