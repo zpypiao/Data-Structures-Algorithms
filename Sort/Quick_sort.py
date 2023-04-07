9de800502d75c842b0bafbca3a111c628059ec22
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-07 15:28:10 +0800

    Update Quick_sort.py

--- Sort/Quick_sort.py
+++ Sort/Quick_sort.py
@@ -1,35 +1,25 @@
 from cal_time import *
 
 def once_sort(li, left, right):
-	mid = (left+right)//2
-	temp = li[mid]
-	while left < right:
-		while True:
-			if li[left]<temp:
-				left += 1
-			else:
-				break
-		while True:
-			if li[right] > temp:
-				right -= 1
-			else:
-				break
-		li[left], li[right] = li[right], li[left]
-	li[left] = temp
-	return left
-			
-
+    temp = li[left]
+    while left < right:
+		while li[right]>=temp and left<right:
+			right -= 1
+		li[left] = li[right]
+        while li[left]<=temp and left < right:
+            left += 1
+        li[right] = li[left]
+    li[left] = temp
+    return left
+            
 @cal_time
 def quick_sort(li, left, right):
-	if left<right:
-		k = once_sort(li, left, right)
-		quick_sort(li, left, k-1)
-		quick_sort(li, k+1, right)
-	else:
-		return None
-		
-li = [int(i) for i in input('Please enter the list:').split()]
-quick_sort(li)
-print(li)
+    if left < right:
+        k = once_sort(li, left, right)
+        quick_sort(li, left, k-1)
+        quick_sort(li, k+1, right)
 
-# this is wrong, it is waitting to be rectified.
+li = [int(i) for i in input('Please enter the list you want to sort:').split()]
+print(li)
+quick_sort(li, 0, len(li)-1)
+print(li)
