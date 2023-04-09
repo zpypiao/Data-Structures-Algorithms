86371df081c8027a2fba22e1efc0fe1c32bae30a
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-09 16:26:50 +0800

    Create 4_solve_maze.py

---
+++ Datastructure/4_solve_maze.py
@@ -0,0 +1,43 @@
+maze = [
+     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+     [1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+     [1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+     [1, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+     [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+     [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+     [1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+     [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+     [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# use stack to solve
+def solve_maze_by_stack(maze, start, end):
+    
+    def search(maze, stack, end):
+        while True:
+            m, n = stack[-1][0], stack[-1][1]
+            if stack[-1] == end:
+                print(stack)
+                return True
+            elif maze[m-1][n] == 0:
+                stack.append((m-1, n))
+                maze[m-1][n]=1
+            elif maze[m][n-1] == 0:
+                stack.append((m, n-1))
+                maze[m][n-1]=1
+            elif maze[m+1][n] == 0:
+                stack.append((m+1, n))
+                maze[m+1][n]=1
+            elif maze[m][n+1] == 0:
+                stack.append((m, n+1))
+                maze[m][n+1]=1
+            else:
+                stack.pop()
+
+    stack = []
+    stack.append(start)
+    maze[start[0]][start[1]]=1
+    search(maze, stack, end)
+
+solve_maze_by_stack(maze, (1,1), (8,8))
