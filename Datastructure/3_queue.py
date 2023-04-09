b078733d065d54255576a81c37255237c7cb1893
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-09 15:14:27 +0800

    Update 3_queue.py

--- Datastructure/3_queue.py
+++ Datastructure/3_queue.py
@@ -1,34 +1,35 @@
 class Queue:
-	def __init__(self, l):
-		self.l = l
-		self.queue = ['' for _ in range(l)]
+	def __init__(self, size):
+		self.size = size
+		self.queue = ['' for _ in range(size)]
 		self.front = 0
-		self.rear = l-1
+		self.rear = 0
 		
 	def move(self, val):
-		if val == self.l-1:
-			return 0
-		else:
-			return val+1
-	def remove(self, val):
-		if val == 0:
-			return self.l - 1
-		else:
-			return val - 1
+		return (val+1)%self.size
 		
 	def join(self, ele):
-		self.rear = self.move(self.rear)
-		self.queue[self.rear] = ele
+		if self.is_filled():
+			print('The queue is filled!')
+		else:
+			self.rear = self.move(self.rear)
+			self.queue[self.rear] = ele
 		
 	def get_front(self):
 		if self.front == self.rear:
-			return None
+			return 'The queue is empty!'
 		else:
 			return self.queue[self.front+1]
 		
 	def pop(self):
-		if self.front == self.rear:
-			return None
-		else:
+		if not self.is_empty():
 			self.front = self.move(self.front)
-			return self.queue[self.remove(self.front)]
+			return self.queue[self.front]
+		else:
+			return 'The queue is empty!'
+		
+	def is_empty(self):
+		return self.front == self.rear
+	
+	def is_filled(self):
+		return (self.rear+1)%self.size == self.front
