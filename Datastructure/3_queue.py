01eb7d2604698b196d4c5c37f4fdc69fd25f351b
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-09 15:27:48 +0800

    Update 3_queue.py

--- Datastructure/3_queue.py
+++ Datastructure/3_queue.py
@@ -1,3 +1,4 @@
+# write by yourself
 class Queue:
 	def __init__(self, size):
 		self.size = size
@@ -33,3 +34,13 @@ class Queue:
 	
 	def is_filled(self):
 		return (self.rear+1)%self.size == self.front
+
+	
+# use the support of python
+from collections import deque
+q = deque([1, 2, 3], 5) # creat a deque which contains 1, 2, 3 and its longth is 5. Be careful, if the longth of the list you input is longer than the longth of queue, the elements in the front will be covered.
+q.append(1) # push an element to right
+q.popleft() # pop an element from left
+
+q.appendleft(1) # push an element to left
+q.pop() # pop an element from right
