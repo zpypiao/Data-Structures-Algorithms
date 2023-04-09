847f7596fe849c55543379035550085fcce5c486
zpypiao <98138915+zpypiao@users.noreply.github.com>
2023-04-09 10:27:11 +0800

    Update 9_bucket_sort.py

--- Sort/9_bucket_sort.py
+++ Sort/9_bucket_sort.py
@@ -6,7 +6,11 @@ def bucket_sort(li, n=100, max_num=10000):
 	for each in li:
 		i = each//step
 		bucket[i].append(each)
-		j = len(bucket[i])-1
+		for j in range(len(bucket[i])-1, 0, -1):
+			if bucket[i][j] < bucket[i][j-1]:
+				bucket[i][j], bucket[i][j-1] = bucket[i][j-1], bucket[j]
+			else:
+				break
 		while j-1>=0 and bucket[i][j] < bucket[i][j-1]:
 			bucket[i][j], bucket[i][j-1] = bucket[i][j-1], bucket[j]
 	i = 0
@@ -14,6 +18,11 @@ def bucket_sort(li, n=100, max_num=10000):
 		for every in each:
 			li[i] = every
 			i += 1
+	sorted_li = []
+	for each in bucket:
+		sorted_li.extend(each)
+	
+	return sorted_li
 			
 li = [random.randint(10000) for _ in range(20)]
 print(li)
